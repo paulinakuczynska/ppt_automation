7db1fe6b1ff78bf01281a82929c416113548b886
paulina <paulina@kuczynska.cc>
2022-06-07 22:58:38 +0200

    zero margins only

--- custom_margins/pptSetMargins.py
+++ custom_margins/pptSetMargins.py
@@ -2,19 +2,12 @@
 
 import argparse, os, shutil
 import xml.etree.cElementTree as ET
-from re import match
 from pathlib import Path
 
 parser = argparse.ArgumentParser(
     allow_abbrev=False, 
     description="File name has to be 'todo.pptx' located in the Desktop"
     )
-
-parser.add_argument("-l", "--left", required=False, nargs="?", default=0, type=float, help="left margin")
-parser.add_argument("-r", "--right", required=False, nargs="?", default=0, type=float, help="right margin")
-parser.add_argument("-t", "--top", required=False, nargs="?", default=0, type=float, help="top margin")
-parser.add_argument("-b", "--bottom", required=False, nargs="?", default=0, type=float, help="bottom margin")
-args = vars(parser.parse_args())
 
 main_path = Path.home() / Path('Desktop')
 end_path = main_path / Path('margins')
@@ -49,10 +42,10 @@ for file in os.listdir(xml_path):
     all_bodyPr = root.findall(f'{tag1}/{tag2}/{tag3}/{tag4}/{tag5}')
     
     for elem in all_bodyPr:
-        elem.set('lIns', str(args["left"]))
-        elem.set('tIns', str(args["top"]))
-        elem.set('rIns', str(args["right"]))
-        elem.set('bIns', str(args["bottom"]))
+        elem.set('lIns', '0')
+        elem.set('tIns', '0')
+        elem.set('rIns', '0')
+        elem.set('bIns', '0')
     
     tree.write(fullname,
             xml_declaration=True,
